3343370d787fa7a64b968ae73ce3516449b972a5
nkpresh <nkwuap@gmail.com>
2023-10-31 11:18:36 +0100

    code and asset update

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -13,6 +13,7 @@ public class Driver : MonoBehaviour
     float steerSpeed = 1;
     [SerializeField]
     float moveSpeed = 0.01f;
+    
     void Start()
     {
 
@@ -21,11 +22,15 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Rotate(0, 0, -steerAmount);
-        transform.Translate(0, moveAmount, 0);
+        // float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        // float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+
+        // transform.Rotate(0, 0, -steerAmount);
+        // transform.Translate(0, moveAmount, 0);
     }
+
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public static InputHandler Instance;
+
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnDirClicked()
+    {
+
+    }
+}
